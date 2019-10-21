ffab97d359c1892269bb77f99805e63a9ff94614
Damien Maillard <damien.maillard@dailymotion.com>
2019-10-21 15:20:57 +0200

    fix exports

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 export { createLogger } from "./src/createLogger/createLogger.js"
 
-// not really meant to be documented or used directly
 export {
   LOG_LEVEL_OFF,
-  LOG_LEVEL_ERROR,
-  LOG_LEVEL_WARN,
+  LOG_LEVEL_DEBUG,
   LOG_LEVEL_INFO,
-  LOG_LEVEL_TRACE,
+  LOG_LEVEL_WARN,
+  LOG_LEVEL_ERROR,
 } from "./src/createLogger/LOG_LEVELS.js"
--- src/createLogger/LOG_LEVELS.js
+++ src/createLogger/LOG_LEVELS.js
@@ -1,3 +1,5 @@
+export const LOG_LEVEL_OFF = "off"
+
 export const LOG_LEVEL_DEBUG = "debug"
 
 export const LOG_LEVEL_INFO = "info"
@@ -5,5 +7,3 @@ export const LOG_LEVEL_INFO = "info"
 export const LOG_LEVEL_WARN = "warn"
 
 export const LOG_LEVEL_ERROR = "error"
-
-export const LOG_LEVEL_OFF = "off"
