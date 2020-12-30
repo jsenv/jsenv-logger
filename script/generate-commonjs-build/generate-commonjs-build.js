b7af11017b85fa634e77ec7689575fe89dcc784f
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-30 17:31:20 +0100

    update dist

--- script/generate-commonjs-build/generate-commonjs-build.js
+++ script/generate-commonjs-build/generate-commonjs-build.js
@@ -4,6 +4,9 @@ import * as jsenvConfig from "../../jsenv.config.js"
 buildProject({
   ...jsenvConfig,
   format: "commonjs",
+  entryPointMap: {
+    "./main.node.js": "./main.cjs",
+  },
   babelPluginMap: getBabelPluginMapForNode(),
   buildDirectoryClean: true,
 })
